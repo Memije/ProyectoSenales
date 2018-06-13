d223fa9e68069d0a7e04ddabf6fadee1d61583bd
Daniel Memije <d.memije26@gmail.com>
2018-06-13 13:01:24 -0500

    Configuracion

---
+++ ProyectoSenales/Configuracion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSenales
+{
+    public static class Configuracion
+    {        
+        public static int[] frecA = { 0, 0, 0, 0 };
+        public static int[] frecE = { 0, 0, 0, 0 };
+        public static int[] frecI = { 0, 0, 0, 0 };
+        public static int[] frecO = { 0, 0, 0, 0 };
+        public static int[] frecU = { 0, 0, 0, 0 };
+
+        public static string puertoCOM = "COM3";
+    }
+}
---
+++ ProyectoSenales/MainForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ProyectoSenales
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grabacionProgressBar = new System.Windows.Forms.ProgressBar();
+            this.testChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.configuraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.establecerFrecuenciasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.testChart)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.grabacionProgressBar);
+            this.groupBox1.Controls.Add(this.testChart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(984, 404);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gráfica de la señal";
+            // 
+            // grabacionProgressBar
+            // 
+            this.grabacionProgressBar.Location = new System.Drawing.Point(6, 19);
+            this.grabacionProgressBar.Name = "grabacionProgressBar";
+            this.grabacionProgressBar.Size = new System.Drawing.Size(972, 10);
+            this.grabacionProgressBar.TabIndex = 0;
+            this.grabacionProgressBar.Visible = false;
+            // 
+            // testChart
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.testChart.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.testChart.Legends.Add(legend6);
+            this.testChart.Location = new System.Drawing.Point(6, 35);
+            this.testChart.Name = "testChart";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "datos";
+            this.testChart.Series.Add(series6);
+            this.testChart.Size = new System.Drawing.Size(972, 353);
+            this.testChart.TabIndex = 0;
+            this.testChart.Text = "chart1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(1002, 78);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(291, 199);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Vocal";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(1002, 283);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(291, 199);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Género";
+            // 
+            // startButton
+            // 
+            this.startButton.FlatAppearance.BorderSize = 0;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Image = global::ProyectoSenales.Properties.Resources.record;
+            this.startButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.startButton.Location = new System.Drawing.Point(12, 27);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(204, 45);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Iniciar Grabación";
+            this.startButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configuraciónToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1304, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // configuraciónToolStripMenuItem
+            // 
+            this.configuraciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.establecerFrecuenciasToolStripMenuItem});
+            this.configuraciónToolStripMenuItem.Name = "configuraciónToolStripMenuItem";
+            this.configuraciónToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.configuraciónToolStripMenuItem.Text = "Configuración";
+            // 
+            // establecerFrecuenciasToolStripMenuItem
+            // 
+            this.establecerFrecuenciasToolStripMenuItem.Name = "establecerFrecuenciasToolStripMenuItem";
+            this.establecerFrecuenciasToolStripMenuItem.Size = new System.Drawing.Size(248, 22);
+            this.establecerFrecuenciasToolStripMenuItem.Text = "Configurar Frecuencias y Puertos";
+            this.establecerFrecuenciasToolStripMenuItem.Click += new System.EventHandler(this.establecerFrecuenciasToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1304, 494);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Reconocedor de Vocales";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.testChart)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ProgressBar grabacionProgressBar;
+        private System.Windows.Forms.DataVisualization.Charting.Chart testChart;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem configuraciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem establecerFrecuenciasToolStripMenuItem;
+    }
+}
+
