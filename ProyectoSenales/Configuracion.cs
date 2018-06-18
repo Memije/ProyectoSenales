a55701cd6184ee2663d713857494025eb290bd0b
Daniel Memije <d.memije26@gmail.com>
2018-06-18 13:10:01 -0500

    Amplitud

--- ProyectoSenales/Configuracion.cs
+++ ProyectoSenales/Configuracion.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 namespace ProyectoSenales
 {
     public static class Configuracion
-    {        
+    {
         public static int[] frecA = { 0, 0, 0, 0 };
         public static int[] frecE = { 0, 0, 0, 0 };
         public static int[] frecI = { 0, 0, 0, 0 };
         public static int[] frecO = { 0, 0, 0, 0 };
         public static int[] frecU = { 0, 0, 0, 0 };
+
+        public static double[] amplitud = { 0.0, 0.0 };
 
         public static string puertoCOM = "COM3";
     }
